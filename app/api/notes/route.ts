e9274ff9ed36fc72ffa2fbb8c4987b44b02abee5
Iryna Paskevych <i.paskevich33@gmail.com>
2026-08-26 19:51:25 +0200

    fix: добавление сохраненных изменений в route.ts

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -34,10 +34,8 @@ export async function GET(request: NextRequest) {
       );
     }
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  } 
   }
-    
-  }
-
 
 export async function POST(request: NextRequest) {
   try {
