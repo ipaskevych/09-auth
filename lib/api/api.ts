da4f812701b26ef9816d9ad4e4a7ad239f1c88b8
Iryna Paskevych <i.paskevich33@gmail.com>
2026-08-28 19:03:39 +0200

    fix: baseURL for vercel

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000') + '/api';
+const baseURL = process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : 'http://localhost:3000/api';
+
 
 export const api = axios.create({
   baseURL,
